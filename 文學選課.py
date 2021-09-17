f40be37c758b3d561f6daf1362086134b4a3456f
fan9704 <cxz123499@gmail.com>
2021-09-17 15:09:32 +0800

    commet add

--- 文學選課.py
+++ 文學選課.py
@@ -8,26 +8,26 @@ from selenium.webdriver.common.keys import Keys
 import schedule
 import time
 
-token="ZQTEJubMouLuf9MgHLoXrhnu0OyIXiZbruIiLP4PWWP"
-drivePath="C:\\Users\a8911\Downloads\chromedriver.exe"
+token="ZQTEJubMouLuf9MgHLoXrhnu0OyIXiZbruIiLP4PWWP"#line notify token
+drivePath="C:\\Users\a8911\Downloads\chromedriver.exe"#chromedriver path
 
-browser=webdriver.Chrome()
+browser=webdriver.Chrome()#open browser
 url='https://webapp.yuntech.edu.tw/AAXCCS/CourseSelectionRegister.aspx'
 
-def lineNotifyMessage_text(token,msg):
+def lineNotifyMessage_text(token,msg):#send message to line notify
     headers={
         "Authorization": "Bearer " + token,
         "Content-Type": "application/x-www-form-urlencoded"
     }
-    payload={'message':msg}
+    payload={'message':msg}#deal message
     print("[System]",time.strftime(" %I:%M:%S %p",time.localtime()), " Line Notify connect complete")
     if(not msg ==''): 
-        r=requests.post("https://notify-api.line.me/api/notify",headers=headers,params=payload)
+        r=requests.post("https://notify-api.line.me/api/notify",headers=headers,params=payload)#post it
         return r.status_code
 
 def course(status):
-    ans=''
-    course_name_list=[]
+    ans=''#create reply answer
+    course_name_list=[]#create all list
     number_list=[]
     teacher_list=[]
     current_student_list=[]
@@ -35,13 +35,13 @@ def course(status):
     browser.get(url)
     # browser.maximize_window()
     
-    courseclass=Select(browser.find_element_by_id('ContentPlaceHolder1_MajOpDDL'))
-    courseclass.select_by_visible_text("文學與創新興趣選項")
+    courseclass=Select(browser.find_element_by_id('ContentPlaceHolder1_MajOpDDL'))#course option
+    courseclass.select_by_visible_text("文學與創新興趣選項")#course option
 
-    button=browser.find_element_by_class_name("buttonGreen")
-    button.click()
+    button=browser.find_element_by_class_name("buttonGreen")#button of send
+    button.click()#click button
     # time.sleep(1)
-    for  a in range(28):#to27
+    for  a in range(28):#to27(course number)
         course_name_ID=str("ContentPlaceHolder1_QueryCourseGridView_CourseEnameLabel_"+str(a))
         course_number_ID=str("ContentPlaceHolder1_QueryCourseGridView_ClassRoomLabel_"+str(a))
         course_teacher_ID=str("ContentPlaceHolder1_QueryCourseGridView_TeacherLabel_"+str(a))
@@ -58,7 +58,7 @@ def course(status):
     print("[System]",time.strftime(" %I:%M:%S %p",time.localtime())," catch course success")
     length=len(number_list)
     for s in range(0,length):
-        if("Creative Thinking" in course_name_list[s] or "Literature and Landscapes" in course_name_list[s]):
+        if("Creative Thinking" in course_name_list[s] or "Literature and Landscapes" in course_name_list[s]):#bind creative thinking and literature and landscapes
             # course_name_list.remove(course_name_list[s])
             # number_list.remove(number_list[s])
             # teacher_list.remove(teacher_list[s])
@@ -66,7 +66,7 @@ def course(status):
             # limit_student_list.remove(limit_student_list[s])
             continue
         # print("課程名稱-",course_name_list[s],"課程代碼-",number_list[s],"授課老師-",teacher_list[s],"修課人數-",current_student_list[s],"修課上限",limit_student_list[s])
-        if(int(current_student_list[s])<int(limit_student_list[s])or not status):
+        if(int(current_student_list[s])<int(limit_student_list[s])or not status):#compare list
             ans+="目前文學與創新課程有空缺!!"
             ans=ans+str("課程名稱-"+course_name_list[s]+"課程代碼-"+number_list[s]+"授課老師-"+teacher_list[s]+"修課人數-"+current_student_list[s]+"修課上限"+limit_student_list[s]+"\n\n")        
     if(ans==''):
@@ -75,23 +75,23 @@ def course(status):
 
 def login():
     try:
-        browser.get('https://webapp.yuntech.edu.tw/YunTechSSO/Account/Login'  ) 
+        browser.get('https://webapp.yuntech.edu.tw/YunTechSSO/Account/Login'  ) #login form
         # ------ address and password ------
-        username = "B10923057"
-        password = "cxz123499"
+        username = "<your SSO username>"
+        password = "<your SSO password>"
         # ------ enter address and password ------
 
         print("[System]",time.strftime(" %I:%M:%S %p",time.localtime())," Login condition prepare function not available please step by")
-        elem = browser.find_element_by_id("pLoginName")
-        elem.send_keys(username)
+        elem = browser.find_element_by_id("pLoginName")#login input
+        elem.send_keys(username)#key in username
         print("[System]",time.strftime(" %I:%M:%S %p",time.localtime())," type in username OK")
-        elem = browser.find_element_by_id("pLoginPassword")
-        elem.send_keys(password)        
+        elem = browser.find_element_by_id("pLoginPassword")#password input
+        elem.send_keys(password)        #key in password
         print("[System]",time.strftime(" %I:%M:%S %p",time.localtime())," type in password OK")
         elem.send_keys(Keys.RETURN)
     except Exception as e:
         print(e)
-        print("[System]",time.strftime(" %I:%M:%S %p",time.localtime()),"has logined")
+        print("[System]",time.strftime(" %I:%M:%S %p",time.localtime()),"has logined")#login condition
     return True
 
 def main():
@@ -116,8 +116,8 @@ def another():
         print("[System]",time.strftime(" %I:%M:%S %p",time.localtime())," ",code)
 
 schedule.every(3).hours.do(another)
-loginstatus=login()
+loginstatus=login()#login event
 schedule.every(0.5).minutes.do(main)
 while True:  
-    schedule.run_pending()  
+    schedule.run_pending()  #scheduling
     time.sleep(1)  
